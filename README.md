3072f2a74be62b1d171e036159e6216a11e9f144
Sivan Mehta <Sivanmehta2@gmail.com>
2017-06-20 15:12:46 -0400

    update pull script and most recent data

---
+++ README.md
@@ -0,0 +1,21 @@
+:cry: Reaccs Only:
+
+Analyzing how people react to the [New York Times on Facebook](https://www.facebook.com/nytimes/). Can we predict how people will react given the text of the message? 
+
+Facebook Graph API limits you only to 100 posts per API call, so I'm just gonna update the dataset in this repo with by-day results via `npm run pull`. On my Macbook, it takes about 2.2 seconds to run:
+
+```
+➜  sad-reaccs-only git:(master) ✗ time npm run pull
+npm info it worked if it ends with ok
+npm info using npm@5.0.0
+npm info using node@v6.10.1
+npm info lifecycle sad-reaccs-only@1.0.0~prepull: sad-reaccs-only@1.0.0
+npm info lifecycle sad-reaccs-only@1.0.0~pull: sad-reaccs-only@1.0.0
+
+> sad-reaccs-only@1.0.0 pull /Users/admin/Documents/Side Projects/sad-reaccs-only
+> ./pull.js > ids/$(date +'%Y-%m-%d').csv
+
+npm info lifecycle sad-reaccs-only@1.0.0~postpull: sad-reaccs-only@1.0.0
+npm info ok
+npm run pull  1.24s user 0.14s system 62% cpu 2.231 total
+```
--- pull.js
+++ pull.js
@@ -16,10 +16,27 @@ FB.api('oauth/access_token', {
   }
   FB.setAccessToken(res.access_token)
 
-  FB.api('/nytimes/feed', 'GET', {"fields":"id","limit":"100"}, (feed) => {
-    console.log("post_id")
+  FB.api('/nytimes/feed', 'GET', { "fields": "created_time,id,message", "limit":"100" }, (feed) => {
+    console.log("post_id\ttimestamp\ttext\tLIKE\tHAHA\tSAD\tLOVE\tANGRY\tWOW\tPRIDE")
     feed.data.map(item => {
-      console.log(item.id)
+      FB.api(item.id + '/reactions', 'GET', {"fields":"type","limit":"1000"}, (response) => {
+        reactions = { LIKE: 0, HAHA: 0, SAD: 0, LOVE: 0, ANGRY: 0, WOW: 0, PRIDE: 0 }
+        response.data.map(react => {
+          reactions[react.type] += 1
+        })
+        console.log(
+          item.id + "\t" +
+          item.created_time + "\t" +
+          escape(item.message) + "\t" +
+          reactions.LIKE + "\t" +
+          reactions.HAHA + "\t" +
+          reactions.SAD + "\t" +
+          reactions.LOVE + "\t" +
+          reactions.ANGRY + "\t" +
+          reactions.WOW + "\t" +
+          reactions.PRIDE + "\t"
+        )
+      })
     })
   })
 })
