db1f9c222b1d5e5acd5882b084685c9885fbc5e5
Sivan Mehta <Sivanmehta2@gmail.com>
2017-07-05 21:34:09 -0400

    added pairwise correlation to network graph

--- analysis/network.R
+++ analysis/network.R
@@ -8,13 +8,23 @@ library(ggraph)
 library(igraph)
 
 # tibble of pairs and their frequencies
-pairs <- common.words %>%
+num.pairs <- common.words %>%
   pairwise_count(word, post_id, sort = TRUE, upper = FALSE) %>%
-  filter(n > 3) %>%
   mutate(pair = paste(item1, item2, sep = "+"))
 
+# tibble of pairs and their correlations
+correlation.pairs <- common.words %>%
+  pairwise_cor(word, post_id, upper = FALSE) %>%
+  mutate(pair = paste(item1, item2, sep = "+"))
+
+pair.combo <- correlation.pairs %>%
+  left_join(num.pairs, by = c('pair')) %>%
+  select(item1.x, item2.x, correlation, pair, n) %>%
+  filter(!is.na(n))
+
 # plot of most common pairs
-pairs %>% 
+pair.combo %>%
+  filter(n > 3) %>%
   ggplot(aes(reorder(pair, n), n)) +
   geom_col() +
   coord_flip() +
@@ -26,11 +36,12 @@ pairs %>%
 
 # network graph of pairs weighted by commonality
 set.seed(1)
-pairs %>%
+pair.combo %>%
+  filter(n > 3) %>%
   graph_from_data_frame() %>%
   ggraph(layout = "kk") +
-  geom_edge_link(aes(edge_alpha = n, edge_width = n), edge_colour = "cyan4") +
-  geom_node_point(size = 5) +
+  geom_edge_link(aes(edge_alpha = n, edge_width = 2), edge_colour = "cyan4") +
+  geom_node_point(size = 2) +
   geom_node_text(aes(label = name), repel = TRUE, 
                  point.padding = unit(0.2, "lines")) +
   theme_void()
