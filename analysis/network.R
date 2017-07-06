93fa587ae77108980a06d7c2ef850329204d2179
Sivan Mehta <Sivanmehta2@gmail.com>
2017-07-05 20:21:52 -0400

    surprise surprise, guess what the most common word pairs are

--- analysis/network.R
+++ analysis/network.R
@@ -1,3 +1,21 @@
 # do something along these lines:
 # http://tidytextmining.com/nasa.html#word-co-ocurrences-and-correlations
 # to find words that appear together
+
+library(dplyr)
+library(widyr)
+
+pairs <- common.words %>%
+  pairwise_count(word, post_id, sort = TRUE, upper = FALSE) %>%
+  filter(n > 5) %>%
+  mutate(pair = paste(item1, item2, sep = "+"))
+
+pairs %>% 
+  ggplot(aes(reorder(pair, n), n)) +
+  geom_col() +
+  coord_flip() +
+  labs(
+    title = "Most common pairs of words in NYT Articles",
+    subtitle = paste("Last", nrow(dat) ,"articles pulled, stop words removed"),
+    y = "# of pairs"
+  )
