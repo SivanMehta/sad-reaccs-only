3562b7962048ff80e35bfcee95eed15df0e42e0e
Sivan Mehta <Sivanmehta2@gmail.com>
2017-07-07 16:36:38 -0400

    add words most commonly paired with trump

--- analysis/network.R
+++ analysis/network.R
@@ -12,17 +12,6 @@ num.pairs <- common.words %>%
   pairwise_count(word, post_id, sort = TRUE, upper = FALSE) %>%
   mutate(pair = paste(item1, item2, sep = "+"))
 
-# plot of most common pairs
-num.pairs %>%
-  filter(n > 3) %>%
-  ggplot(aes(x = reorder(pair, n), y = n)) +
-  geom_col() +
-  coord_flip() +
-  labs(
-    title = "Most common pairs of words in NYT Articles",
-    subtitle = paste("Last", nrow(dat) ,"articles pulled, stop words removed"),
-    y = "# of pairs")
-
 # network graph of pairs weighted by commonality
 set.seed(1)
 num.pairs %>%
@@ -34,3 +23,26 @@ num.pairs %>%
   geom_node_text(aes(label = name), repel = TRUE, 
                  point.padding = unit(0.2, "lines")) +
   theme_graph()
+
+# plot of most common pairs
+num.pairs %>%
+  filter(n > 3) %>%
+  ggplot(aes(x = reorder(pair, n), y = n)) +
+  geom_col() +
+  coord_flip() +
+  labs(
+    title = "Most common pairs of words in NYT headlines",
+    subtitle = paste("Last", nrow(dat) ,"articles pulled, stop words removed"),
+    y = "# of pairs")
+
+# OK lets cut straight to the chase, what about the Donald?
+num.pairs %>% 
+  filter(item1 == "trump" | item2 == "trump") %>%
+  filter(n > 2) %>%
+  arrange(-n) %>%
+  ggplot(aes(x = reorder(pair, n), y = n)) +
+  geom_col() + coord_flip() +
+  labs(
+    title = "Words most commonly paired with \"Trump\" in NYT headlines",
+    subtitle = paste("Last", nrow(dat) ,"articles pulled, stop words removed"),
+    y = "# of pairs")
